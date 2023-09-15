dfc07e17810e8d3c4cdc46ca09d7f0411f9b2e76
tai <eduardotaidsr@gmail.com>
2023-09-15 12:11:15 -0300

    fix: identation is now correct and also added the option of numbers the user can choose in the prompt

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
-let answer = parseInt(prompt("Please enter the number of cells in each row and column: "));
+let answer = parseInt(prompt("Please enter the number of cells (16, 32 or 64) in each row and column: "));
 let maxWidth = 600; // O tamanho do container em pixels
-
 
 function addGridSize(size) {
     const container = document.querySelector('#container');
 
     // Configura o container como uma grid
-    container.style.width = maxWidth + 'px';
-    container.style.height = maxWidth + 'px';
+    container.style.width = `${maxWidth}px`;
+    container.style.height = `${maxWidth}px`;
     container.style.display = 'grid';
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
     let cellSize = (maxWidth / size) - 2; // Subtrai 2px devido à borda de 1px em cada lado
-
 
     // Primeiro loop para as linhas
     for (let i = 1; i <= size; i++) {
@@ -22,40 +19,32 @@ function addGridSize(size) {
         for (let j = 1; j <= size; j++) {
             const cell = document.createElement('div');
             cell.classList.add('grid');
-            cell.style.width = cellSize + 'px';
-            cell.style.height = cellSize + 'px';
+            cell.style.width = `${cellSize}px`;
+            cell.style.height = `${cellSize}px`;
             cell.style.background = 'white';
             cell.style.border = '1px solid black';
             container.appendChild(cell);
 
-
-            ["click", 'mousedown', 'mousemove', 'mouseup'].forEach(ev=>{
-                cell.addEventListener(ev, function(e){
-                   if(ev=="click"){
-                    cell.style.background = 'red'
-                   }
-
-                   cell.addEventListener('mousemove', function(event) {
-                    if(event.buttons == 1) {
-                     event.preventDefault();
-                        cell.style.background = 'red'
+            ["click", 'mousedown', 'mousemove', 'mouseup'].forEach(ev => {
+                cell.addEventListener(ev, function(e) {
+                    if (ev === "click") {
+                        cell.style.background = 'red';
                     }
-                   });
-                   
-            });
+
+                    cell.addEventListener('mousemove', function(event) {
+                        if (event.buttons === 1) {
+                            event.preventDefault();
+                            cell.style.background = 'red';
+                        }
+                    });
                 });
+            });
         }
-    }  
-    
+    }
 }
-
-
-
 
 if ([16, 32, 64].includes(answer)) {
     addGridSize(answer);
 } else {
     alert("Invalid size. Please enter either 16, 32, or 64.");
 }
-
-
